9790c0d9ba595af4efa87da2ae330e52fab39074
facenach <ignacioijg@gmail.com>
2026-08-20 17:37:45 -0300

    added conn pool for posgres

--- vector/store.go
+++ vector/store.go
@@ -13,6 +13,7 @@ type Result struct {
 	Document
 	Score float32
 }
+
 type Store interface {
 	Upsert(ctx context.Context, docs []Document) error
 	Query(ctx context.Context, embedding []float32, topK int) ([]Result, error)
